ccdd4d230f025bebe7dea1a9902f70aed7715930
=O.Mohamed <=amedhom@gmail.com>
2022-05-30 20:56:49 +0100

    fix maintenance_type apis

--- routers/type_maintenance_router.js
+++ routers/type_maintenance_router.js
@@ -7,83 +7,88 @@ const typeMaintenanceRouter = express.Router();
 const { typeMaintenance } = new PrismaClient();
 
 //tout les types de maintenance
-typeMaintenanceRouter.get('/', async (req, res) => {
-  try {
-    const typeMaintenances = await typeMaintenance.findMany();
-    res.status(200).send(typeMaintenances);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
+typeMaintenanceRouter.get('/', async(req, res) => {
+    try {
+        const typeMaintenances = await typeMaintenance.findMany({
+            orderBy: {
+                type: 'asc',
+            }
+        }, );
+        res.status(200).send(typeMaintenances);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 });
 
 //Ajouter un typemaintenance
-typeMaintenanceRouter.post('/', async (req, res) => {
-  try {
-    const { type } = req.body;
+typeMaintenanceRouter.post('/', async(req, res) => {
+    try {
+        const { type } = req.body;
 
-    const createdTypeMaintenance = await typeMaintenance.createMany({
-      data: [
-        {
-          type,
-        },
-      ],
-    });
-    res.status(201).send(createdTypeMaintenance);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
+        const createdTypeMaintenance = await typeMaintenance.createMany({
+            data: [{
+                type,
+            }, ],
+        });
+        res.status(201).send(createdTypeMaintenance);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 });
 
-//modifier un véhicule
-typeMaintenanceRouter.put('/', async (req, res) => {
-  try {
-    const { id, type } = req.body;
-    const typeMaintenceExist = await typeMaintenance.findUnique({
-      where: {
-        id: Number(id),
-      },
-    });
-    if (typeMaintenceExist) {
-      const updatedTypeMaintenance = await typeMaintenance.update({
-        where: {
-          id,
-        },
-        data: {
-          type,
-        },
-      });
-      res.status(200).send(updatedTypeMaintenance);
-    } else {
-      res.status(404).send('typemaintenance not found');
+//modifier un type
+typeMaintenanceRouter.put('/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+
+        const { type } = req.body;
+        const typeMaintenceExist = await typeMaintenance.findUnique({
+            where: {
+                id: Number(id),
+            },
+        });
+        if (typeMaintenceExist) {
+            const updatedTypeMaintenance = await typeMaintenance.update({
+                where: {
+                    id: Number(id),
+
+                },
+                data: {
+                    type,
+                },
+            });
+            res.status(200).send(updatedTypeMaintenance);
+        } else {
+            res.status(404).send('typemaintenance not found');
+        }
+    } catch (error) {
+        res.status(500).send(error.message);
     }
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
 });
 
 //supprimer une typemaintenance
-typeMaintenanceRouter.delete('/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
+typeMaintenanceRouter.delete('/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
 
-    const typeMaintenanceExist = await typeMaintenance.findUnique({
-      where: {
-        id: Number(id),
-      },
-    });
-    if (typeMaintenanceExist) {
-      const deletedTypeMaintenance = await typeMaintenance.delete({
-        where: {
-          id: Number(id),
-        },
-      });
-      res.status(200).send('typemaintenance deleted');
-    } else {
-      res.status(404).send('typemaintenance not found');
+        const typeMaintenanceExist = await typeMaintenance.findUnique({
+            where: {
+                id: Number(id),
+            },
+        });
+        if (typeMaintenanceExist) {
+            const deletedTypeMaintenance = await typeMaintenance.delete({
+                where: {
+                    id: Number(id),
+                },
+            });
+            res.status(200).send('typemaintenance deleted');
+        } else {
+            res.status(404).send('typemaintenance not found');
+        }
+    } catch (error) {
+        res.status(500).send(error.message);
     }
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
 });
 
 export default typeMaintenanceRouter;
